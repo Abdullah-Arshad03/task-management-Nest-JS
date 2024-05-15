6fdd863e5ab1a0a012862b4c6732bd36923fb8b2
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2024-05-15 20:58:46 +0500

    applied the validation on the filtering and searching too

--- src/tasks/dto/get-task-filter.dto.ts
+++ src/tasks/dto/get-task-filter.dto.ts
@@ -1,7 +1,14 @@
 import { TaskStatus } from "../tasks.model";
+import {IsOptional , IsEnum, IsString} from 'class-validator'
+
 
 export class GetTaskFilterDto {
+    @IsOptional()
+    @IsEnum(TaskStatus)
     status?: TaskStatus
+
+   @IsOptional()
+   @IsString()
     search?: string
 }
 
--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,7 +1,10 @@
+import { IsEnum } from "class-validator"
 import { TaskStatus } from "../tasks.model"
 
 export class UpdateTask {
     title? : string 
     description? : string
+
+    @IsEnum(TaskStatus)
     taskStatus : TaskStatus
 }
